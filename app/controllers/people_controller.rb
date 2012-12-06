109dc39a3b3ac6e366286b1328bad535bd686ca7
Luiz Faias Junior <luizfaias@gmail.com>
2012-12-06 12:40:09 -0200

    Corrigindo status no cadastro de usuários

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,12 @@
-class PeopleController  < InheritedResources::Base
+class PeopleController < InheritedResources::Base
 
   def index
     @people = Person.search(params[:query], params[:page])
+  end
+  
+  def new
+    @status = true
+    super
   end
   
   def emails
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,6 @@
 class Person < ActiveRecord::Base
 
   validates_length_of :name, :minimum => 7
-  validates_presence_of :name, :gender, :birth_date, :home_phone
   
   default_scope order('name')
   scope :active, where(:status => true)
